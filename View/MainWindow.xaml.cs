68579b093a7cec30d18ac76055d3c291f4b5f742
NhatNS <nsn251tp@gmail.com>
2025-07-17 21:52:16 +0700

    delete unused project

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Service;
+using ViewModel;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(AuthenticationService authenticationService)
+        {
+            InitializeComponent();
+            MainContent.Content = new LoginView(authenticationService);
+            //Content = new ResetPasswordView(authenticationService, "acsacac");
+            //MainContent.Content = new DoctorSchedulesView(doctorScheduleService);
+        }
+    }
+}
